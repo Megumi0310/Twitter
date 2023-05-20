d68775b8fe1bdd3f40cfc04eba6e57d1f8973de1
Megumi0310 <132648399+Megumi0310@users.noreply.github.com>
2023-05-20 17:17:26 +0800

    Initial commit

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    //
+    public function showTimelinePage()
+    {
+			//latest関数：テーブル内のレコードを作成日時の降順で取得する
+			$tweets = Tweet::latest()->get();
+      return view('timeline',['tweets' => $tweets]);
+    }
+    //formでデータを送信する際は、$requestの中にデータを入れて送信する
+    public function postTweet(Request $request)
+    {
+			// dd($request);
+			$validator = $request->validate([
+				'tweet' => ['required','string','max:280'],
+			]);
+			
+        Tweet::create([
+        'user_id' => 1,
+        'tweet' => $request->tweet,
+         ]);
+
+				 return back();
+	}
+}
